163e2041f1bcb566211a6cd7c4c77e5d9f1656b8
Joao Neto <neto31510@gmail.com>
2026-01-02 13:36:46 -0300

    Criando a classe Posicao

--- src/aplicacao/ProgramaPrincipal.java
+++ src/aplicacao/ProgramaPrincipal.java
@@ -1,10 +1,14 @@
 package aplicacao;
 
+import jogodetabuleiro.Posicao;
+
 public class ProgramaPrincipal {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Olá Mundo!");
+		Posicao posicao = new Posicao(3, 5);
+		
+		System.out.println(posicao);
 
 	}
 
