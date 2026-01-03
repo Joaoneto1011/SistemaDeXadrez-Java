bd4581171bcade08b5ba8ef64add0ef58daadb7d
Joao Neto <neto31510@gmail.com>
2026-01-03 17:45:08 -0300

    Tratando excecoes e limpando a tela

--- src/aplicacao/ProgramaPrincipal.java
+++ src/aplicacao/ProgramaPrincipal.java
@@ -1,7 +1,9 @@
 package aplicacao;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import xadrez.ExcecoesXadrez;
 import xadrez.PartidaDeXadrez;
 import xadrez.PecaDeXadrez;
 import xadrez.PosicaoXadrez;
@@ -11,21 +13,30 @@ public class ProgramaPrincipal {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		
 		PartidaDeXadrez partidaDeXadrez = new PartidaDeXadrez();
 		
 		while (true) {
-			InterfaceDoUsuario.printTabuleiro(partidaDeXadrez.getPecas());
-			System.out.println();
-			System.out.print("Posicao de origem: ");
-			PosicaoXadrez origem = InterfaceDoUsuario.leituraPosicaoXadrez(sc);
-			
-			System.out.println();
-			System.out.print("Posicao de destino: ");
-			PosicaoXadrez destino = InterfaceDoUsuario.leituraPosicaoXadrez(sc);
-			
-			PecaDeXadrez pecaCapturada = partidaDeXadrez.execucaoMovimentoXadrez(origem, destino);
-					
+			try {
+				InterfaceDoUsuario.limparTela();
+				InterfaceDoUsuario.printTabuleiro(partidaDeXadrez.getPecas());
+				System.out.println();
+				System.out.print("Posicao de origem: ");
+				PosicaoXadrez origem = InterfaceDoUsuario.leituraPosicaoXadrez(sc);
+				
+				System.out.println();
+				System.out.print("Posicao de destino: ");
+				PosicaoXadrez destino = InterfaceDoUsuario.leituraPosicaoXadrez(sc);
+				
+				PecaDeXadrez pecaCapturada = partidaDeXadrez.execucaoMovimentoXadrez(origem, destino);
+			}
+			catch (ExcecoesXadrez e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 		
 
